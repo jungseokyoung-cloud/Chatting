199a704f919bfc9c71a9a94010843eab4b455f8a
jungseokyoung-cloud <thrdud0423@gmail.com>
2023-05-18 20:31:34 +0900

    [DOCS] : Project추가 및 GitIgnore추가

---
+++ Chatting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chatting
+//
+//  Created by Seok Young Jung on 2023/05/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
